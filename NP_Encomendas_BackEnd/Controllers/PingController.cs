e7a511c0e8d503ea8a10eae337b1ce87b64f7f62
vanilson <vanilson.liins@gmail.com>
2026-05-06 12:29:01 -0300

    adiciononando um controller para receber ping  do cronjob

---
+++ NP_Encomendas_BackEnd/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NP_Encomendas_BackEnd.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Ping()
+    {
+        return Ok("O ping funcionou com sucesso");
+    }
+}
